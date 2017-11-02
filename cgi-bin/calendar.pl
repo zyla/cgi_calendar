73f63a37c4e21941b1712584cf4a85fca5f62eff
Maciej Bielecki <zyla@prati.pl>
2017-11-02 16:30:20 +0100

    Add data insertion

--- cgi-bin/calendar.pl
+++ cgi-bin/calendar.pl
@@ -6,7 +6,7 @@ use DBI;
 use CGI;
 use CGI::Carp qw(fatalsToBrowser);
 use MIME::Base64;
-use POSIX qw(strftime);
+use POSIX qw(strftime mktime);
 
 my $DATABASE_FILENAME = "db.sqlite3";
 
@@ -56,13 +56,28 @@ if(!$authorization) {
 	}
 }
 
-sub format_date {
+# userdate - string, "YYYY-MM-DD hh:mm"
+# timestamp - seconds since Unix epoch
+
+# Parse userdate.
+# Returns undef on failure.
+sub userdate_to_timestamp {
 	my $date = shift @_;
-	$date =~ s/T/ /;
-	return $date;
+	if($date =~ /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})$/) {
+		return mktime(0, $4, $3, $2, $1, $0);
+	} else {
+		return undef;
+	}
+}
+
+sub timestamp_to_userdate {
+	my $time = shift @_;
+	return strftime("%F %R", localtime($time));
 }
 
 sub page_view_calendar {
+	print $q->header("text/html; charset=utf-8");
+
 	print $q->h1("Kalendarz - $user{full_name}");
 
 	my $st = $dbh->prepare("SELECT * FROM calendar_entries WHERE user_login = ?");
@@ -70,7 +85,7 @@ sub page_view_calendar {
 
 	while(my $row = $st->fetchrow_hashref) {
 		print "<div>";
-		print $q->p(format_date($row->{date_from}) . " &ndash; " . format_date($row->{date_to}));
+		print $q->p($row->{date_from} . " &ndash; " . $row->{date_to});
 		print $q->p($entry_type_descriptions{$row->{entry_type}});
 		print "</div>";
 	}
@@ -78,7 +93,68 @@ sub page_view_calendar {
 	print q(<a href="?page=add_entry">Dodaj nowy wpis</a>);
 }
 
+sub parse_add_entry_request {
+	my $errors = shift @_;
+
+	my $entry_type = $q->param('entry_type');
+	unless($entry_type && grep { $_ == $entry_type } (keys %entry_type_descriptions)) {
+		push @$errors, "Nieprawidłowy typ";
+	}
+
+	my $date_from = $q->param('date_from');
+	my $date_to_ts;
+	unless(my $date_from_ts = userdate_to_timestamp($date_from)) {
+		push @$errors, "Nieprawidłowa data rozpoczęcia";
+	}
+
+	my $date_to = $q->param('date_to');
+	my $date_from_ts;
+	unless($date_to_ts = userdate_to_timestamp($date_to)) {
+		push @$errors, "Nieprawidłowa data zakończenia";
+	}
+
+	if($date_from_ts && $date_to_ts && $date_to_ts le $date_from_ts) {
+		push @$errors, "Data zakończenia powinna być późniejsza niż data zakończenia";
+	}
+
+	if(@$errors) {
+		return undef;
+	} else {
+		return (
+			"entry_type" => $entry_type,
+			"date_from" => $date_from,
+			"date_to" => $date_to
+		);
+	}
+}
+
+sub save_entry {
+	my $entry = shift @_;
+	my $st = $dbh->prepare("INSERT INTO calendar_entries(user_login, entry_type, date_from, date_to) VALUES (?, ?, ?, ?)");
+	$st->execute(
+		$entry->{user_login},
+		$entry->{entry_type},
+		$entry->{date_from},
+		$entry->{date_to});
+}
+
 sub page_add_entry {
+	my @errors = ();
+
+	if($ENV{REQUEST_METHOD} eq "POST") {
+		my %record = parse_add_entry_request(\@errors);
+		if(!@errors) {
+			$record{user_login} = $user{login};
+			save_entry(\%record, \@errors);
+			if(!@errors) {
+				print $q->redirect("?page=view");
+				exit;
+			}
+		}
+	}
+
+	print $q->header("text/html; charset=utf-8");
+
 	print $q->h1("Nowy wpis - $user{full_name}");
 
 	print $q->start_form(-method => "POST");
@@ -88,16 +164,19 @@ sub page_add_entry {
           'work', \%entry_type_descriptions);
 	my $start_time = int(time / 3600) * 3600 + 3600;
 	print $q->p, $q->label('Data początkowa: '),
-		$q->textfield('date_from', strftime("%F %R", localtime($start_time)));
+		$q->textfield('date_from', timestamp_to_userdate($start_time));
 	print $q->p, $q->label('Data końcowa: '),
-		$q->textfield('date_to', strftime("%F %R", localtime($start_time + 7200)));
+		$q->textfield('date_to', timestamp_to_userdate($start_time + 7200));
+
+	foreach(@errors) {
+		print "<p style=\"color: red;\">$_</p>";
+	}
+		
 	print $q->p, $q->submit(-value => "Dodaj");
 	print $q->end_form();
 }
 
 my $page = $q->param("page");
-
-print $q->header("text/html; charset=utf-8");
 
 my %pages = (
 	"view" => sub { page_view_calendar(); },
--- data.sql
+++ data.sql
@@ -2,9 +2,9 @@ INSERT INTO users (login,password,full_name,user_type) VALUES ('szef', 'hunter2'
 INSERT INTO users (login,password,full_name,user_type) VALUES ('jan', '123', 'Jan Kowalski', 'empl');
 
 INSERT INTO calendar_entries(user_login, entry_type, date_from, date_to) VALUES
-  ('szef', 'work', '2017-11-02T09:00:00', '2017-11-02T15:00:00'),
-  ('szef', 'work', '2017-11-03T09:00:00', '2017-11-03T14:00:00'),
+  ('szef', 'work', '2017-11-02 09:00', '2017-11-02 15:00'),
+  ('szef', 'work', '2017-11-03 09:00', '2017-11-03 14:00'),
 
-  ('jan', 'work', '2017-11-02T07:00:00', '2017-11-02T22:00:00'),
-  ('jan', 'work', '2017-11-03T07:00:00', '2017-11-03T22:00:00')
+  ('jan', 'work', '2017-11-02 07:00', '2017-11-02 22:00'),
+  ('jan', 'work', '2017-11-03 07:00', '2017-11-03 22:00')
   ;
